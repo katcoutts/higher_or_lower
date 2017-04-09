58486971dcaff3d533b6f1bd5e0250a9b8323c3b
Katrina Coutts <katcoutts@hotmail.co.uk>
2017-04-09 14:00:40 +0100

    fixes number comparison bug

--- index.android.js
+++ index.android.js
@@ -3,6 +3,8 @@
  * https://github.com/facebook/react-native
  * @flow
  */
+// GETTING AN ERROR WHERE IT'S COMPARING SINGLE DIGIT AND DOUBLE DIGITS WRONGLY. SO EG. IT DOESN'T THINK 5 IS SMALLER THAN 41 - SEEMS TO BE COMPARING JUST THE FIRST NUMBER (IE 5 WITH 4)
+
 
 import React, { Component } from 'react';
 import renderIf from './renderIf';
@@ -23,34 +25,46 @@ export default class HigherOrLower extends Component {
     this.state = {
       number: number,
       score: 0,
+      wrong: false,
+      draw: false,
     };
   }
 
   onHigherButtonPress() {
     newNumber = (Math.random() * 100).toFixed();
-    if (newNumber > this.state.number) {
+    if (+newNumber > +this.state.number) {
       this.setState({number: newNumber});
       this.state.score += 1;
-    } else {
-      this.setState({number: "You're wrong. It was " + newNumber});
+    } 
+    else if (+newNumber == +this.state.number){
+      this.setState({number: newNumber, wrong: true, draw: true})
+    }
+    else {
+      this.setState({number: newNumber, wrong: true});
     }
   }
 
   onLowerButtonPress() {
     newNumber = (Math.random() * 100).toFixed();
-    if (newNumber < this.state.number) {
+    if (+newNumber < +this.state.number) {
       this.setState({number: newNumber});
       this.state.score += 1;
-    } else {
-      this.setState({number: "You're wrong. It was " + newNumber});
+    } 
+    else if (+newNumber == +this.state.number){
+      this.setState({number: newNumber, wrong: true, draw: true})
+    }
+    else {
+      this.setState({number: newNumber, wrong: true});
     }
   }
 
   onResetButtonPress() {
-    number = (Math.random() * 100).toFixed();
+    newNumber = (Math.random() * 100).toFixed();
     this.setState({
-      number: number,
+      number: newNumber,
       score: 0,
+      wrong: false,
+      draw: false,
     })
   }
 
@@ -60,17 +74,33 @@ export default class HigherOrLower extends Component {
         <Text style={styles.welcome}>
           Higher or Lower?
         </Text>
-        <Text style={styles.number}>
-          {this.state.number}
-        </Text>
+        {renderIf(!this.state.wrong,
+          <Text style={styles.number}>
+            {this.state.number}
+          </Text>
+        )}
+        {renderIf(this.state.wrong && !this.state.draw,
+          <Text style={styles.wrong}>
+            You're wrong. It was {this.state.number}
+          </Text>
+        )}
+        {renderIf(this.state.draw,
+          <Text style={styles.wrong}>
+            Unlucky. It's {this.state.number} again!
+          </Text>
+        )}
+        {renderIf(!this.state.wrong,
         <View style={styles.buttonContainer}>
           <Button title="Higher" onPress={this.onHigherButtonPress.bind(this)}/>
           <Button title="Lower" onPress={this.onLowerButtonPress.bind(this)}/>
         </View>
+        )}
         <Text style={styles.score}>
           Your score: {this.state.score}
         </Text>
+        {renderIf(this.state.wrong,
         <Button title="Reset" onPress={this.onResetButtonPress.bind(this)} />
+        )}
       </View>
     );
   }
@@ -93,6 +123,12 @@ const styles = StyleSheet.create({
   number:{
     fontSize: 70,
     color: 'blue',
+    textAlign: 'center',
+    margin: 40,
+  },
+  wrong:{
+    fontSize: 50,
+    color: 'tomato',
     textAlign: 'center',
     margin: 40,
   },
